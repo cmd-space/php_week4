6281260687e549699308609813db4d8f5f68be1a
Mason Crane <MEC@Masons-MacBook-Pro.local>
2015-03-27 14:02:01 -0600

    Done with sorting algo 1

--- sorting_algos/sorting1.php
+++ sorting_algos/sorting1.php
@@ -8,6 +8,21 @@
 		{
 			$array1[] = rand(0,10000);
 		}
+
+		for ($j = 0; $j < count($array1); $j++)
+		{
+			$temp = 0;
+
+			for ($k = 0; $k < count($array1); $k++)
+			{
+				if ($array1[$j] < $array1[$k])
+				{
+					$temp = $array1[$j];
+					$array1[$j] = $array1[$k];
+					$array1[$k] = $temp;
+				}
+			}
+		}
 		var_dump($array1);
 	}
 
