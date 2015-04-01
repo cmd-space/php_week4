07e6a69a06b63b885678f9052b60dd157de72975
Mason Crane <MEC@Masons-MacBook-Pro.local>
2015-03-31 20:13:54 -0600

    working on ninja gold game, nearly finished

---
+++ advanced_chapter/ninja_gold/index.php
@@ -0,0 +1,130 @@
+<?php
+
+	session_start();
+
+	if (!isset($_SESSION['gold']))
+	{
+		$_SESSION['gold'] = 0;
+	}
+	if (!isset($_SESSION['activities']))
+	{
+		$_SESSION['activities'] = array();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Make Money!!!</title>
+	<style>
+		body
+		{
+			font-family: sans-serif;
+		}
+		#gold h1
+		{
+			display: inline-block;
+			margin-left: 2em;
+		}
+		#gold span
+		{
+			padding: .25em;
+			border: 2px solid black;
+		}
+		#gold form
+		{
+			display: inline-block;
+			padding-left: 55%;
+		}
+		.border
+		{
+			width: 15em;
+			height: 13em;
+			border: 2px solid black;
+			text-align: center;
+			padding: 0em 1em 1em 1em;
+			display: inline-block;
+			margin-top: 1em;
+			margin-left: 2em;
+		}
+		#activity
+		{
+			margin-top: 2em;
+			margin-left: 2em;
+		}
+		#activities
+		{
+			width: 74.5em;
+			height: 12em;
+			overflow: scroll;
+			padding: .5em .5em;
+			border: 2px solid black;
+		}
+		.fail
+		{
+			color: red;
+		}
+		.success
+		{
+			color: green;
+		}
+	</style>
+</head>
+<body>
+	<div id='gold'>
+		<h1>Your Gold:</h1>
+		<h1><span><?= $_SESSION['gold'] ?></span></h1>
+		<form action="process.php" method="post">
+			<input type="hidden" name="restart" value="reset" />
+			<input type="submit" value="Restart Game" />
+		</form>
+	</div>
+	<div>
+		<div class='border'>
+			<h1>Farm</h1>
+			<h2>(earns 10-20 gold)</h2>
+			<form action="process.php" method="post">
+				<input type="hidden" name="building" value="farm" />
+				<input type="submit" value="Find Gold!" />
+			</form>
+		</div>
+		<div class='border'>
+			<h1>Cave</h1>
+			<h2>(earns 5-10 gold)</h2>
+			<form action="process.php" method="post">
+				<input type="hidden" name="building" value="cave" />
+				<input type="submit" value="Find Gold!" />
+			</form>
+		</div>
+		<div class='border'>
+			<h1>House</h1>
+			<h2>(earns 2-5 gold)</h2>
+			<form action="process.php" method="post">
+				<input type="hidden" name="building" value="house" />
+				<input type="submit" value="Find Gold!" />
+			</form>
+		</div>
+		<div class='border'>
+			<h1>Casino!</h1>
+			<h2>(earns/takes 0-50 gold)</h2>
+			<form action="process.php" method="post">
+				<input type="hidden" name="building" value="casino" />
+				<input type="submit" value="Find Gold!" />
+			</form>
+		</div>
+	</div>
+	<div id='activity'>
+		<h3>Activities:</h3>
+		<div id='activities'>
+			<?php
+				foreach ($_SESSION['activities'] as $activity) 
+				{
+					echo  $activity;
+				}
+			?>
+		</div>
+	</div>
+</body>
+</html>
---
+++ advanced_chapter/ninja_gold/process.php
@@ -0,0 +1,51 @@
+<?php
+
+	session_start();
+
+	date_default_timezone_set('America/Denver');
+
+	if (!empty($_POST['restart']))
+	{
+		session_destroy();
+		header('location: index.php');
+		die();
+	}
+
+	if ($_POST['building'] == 'farm')
+	{
+		$gold = rand(10,20);
+	}
+	if ($_POST['building'] == 'cave')
+	{
+		$gold = rand(5,10);
+	}
+	if ($_POST['building'] == 'house')
+	{
+		$gold = rand(2,5);
+	}
+	if ($_POST['building'] == 'casino')
+	{
+		$dice = rand(0,1);
+		if ($dice == 1)
+		{
+			$gold = rand(0,50);
+		}
+		else
+		{
+			$gold = rand(0,50);
+			$gold *= -1;
+		}
+	}
+	$_SESSION['gold'] += $gold;
+	if ($gold < 0)
+	{
+		$_SESSION['activities'][] = '<p class="fail">You entered a ' . $_POST['building'] . ' and lost ' . abs($gold) . ' gold... Ouch..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(' . date("F jS Y g:i a") . ')</p>';
+	}
+	else
+	{
+		$_SESSION['activities'][] = '<p class="success">You entered a ' . $_POST['building'] . ' and earned ' . $gold . ' gold.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(' . date("F jS Y g:i a") . ')</p>';
+	}
+	header('location: index.php');
+	die();
+
+?>
